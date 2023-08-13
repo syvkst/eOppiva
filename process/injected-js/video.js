c49de58e1fd2d7b317c865cde41816698579d744
Willowfang <68636441+CodeX-fi@users.noreply.github.com>
2023-08-13 18:51:44 +0300

    Improvements

--- process/injected-js/video.js
+++ process/injected-js/video.js
@@ -1,5 +1,5 @@
 const videoRules =
-  "[data-sytheme=dark]{--sy_video_button_bg:var(--sy_level_2);--sy_video_button_fg:var(--sy_font_color);--sy_video_button_hover_bg:var(--sy_level_3);--sy_video_button_hover_fg:var(--sy_blue_regular)}[data-sytheme=light]{--sy_video_button_bg:var(--sy_light);--sy_video_button_fg:var(--sy_font_color);--sy_video_button_hover_bg:var(--sy_grey_A_regular);--sy_video_button_hover_fg:var(--sy_light)}.video-js{display:none}.video-button{color:var(--sy_video_button_fg);width:100%;padding:2rem 2rem;font-size:100%;background-color:var(--sy_video_button_bg);border-radius:.25rem;box-shadow:var(--sy_shadow_1_x) var(--sy_shadow_1_y) var(--sy_shadow_1_blur) var(--sy_shadow_1_spread) var(--sy_shadow_1_color),var(--sy_shadow_2_x) var(--sy_shadow_2_y) var(--sy_shadow_2_blur) var(--sy_shadow_2_spread) var(--sy_shadow_2_color);transition:.2s}.video-button:hover{background-color:var(--sy_video_button_hover_bg);color:var(--sy_video_button_hover_fg)}@media (min-width:767.98px){.video-js{display:block}.video-button{display:none}}";
+  "[data-sytheme=dark]{--sy_video_button_bg:var(--sy_level_2);--sy_video_button_fg:var(--sy_font_color);--sy_video_button_hover_bg:var(--sy_level_3);--sy_video_button_hover_fg:var(--sy_blue_regular)}[data-sytheme=light]{--sy_video_button_bg:var(--sy_grey_A_regular);--sy_video_button_fg:var(--sy_light);--sy_video_button_hover_bg:var(--sy_grey_A_400);--sy_video_button_hover_fg:var(--sy_grey_A_800)}.video-js{display:none}.video-button{display:block;width:100%;height:14rem;font-size:6rem;padding:2rem 2rem;color:var(--sy_video_button_fg);background-color:var(--sy_video_button_bg);border-radius:.25rem;box-shadow:var(--sy_shadow_1_x) var(--sy_shadow_1_y) var(--sy_shadow_1_blur) var(--sy_shadow_1_spread) var(--sy_shadow_1_color),var(--sy_shadow_2_x) var(--sy_shadow_2_y) var(--sy_shadow_2_blur) var(--sy_shadow_2_spread) var(--sy_shadow_2_color);transition:.2s}.video-button i{color:inherit}.video-button:hover{background-color:var(--sy_video_button_hover_bg);color:var(--sy_video_button_hover_fg)}@media (min-width:767.98px){.video-js{display:block}.video-button{display:none}}";
 
 function initializeVideos() {
   const videos = document.querySelectorAll(".video-js");
@@ -13,7 +13,7 @@ function initializeVideos() {
     playbackButton.target = "_blank";
 
     let playbackIcon = document.createElement("i");
-    playbackIcon.classList.add("fa", "fa-video-camera");
+    playbackIcon.classList.add("fa", "fa-play-circle");
     playbackButton.appendChild(playbackIcon);
 
     video.parentElement.insertBefore(playbackButton, video);
